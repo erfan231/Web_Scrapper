175b6843a54e390912327ad564dbec04399f7ef4
erfan231 <mahmoodierfan7@gmail.com>
2020-10-01 15:14:30 +0800

    First commit

--- main.py
+++ main.py
@@ -22,21 +22,15 @@ for n in range(1,51): #pages(1 to 50)
         #so we search the <a> tag and grab the title from it
             Book_titles.append(book_title)
             
-
 num = 0
-
 for B in Book_titles:
     for P in Book_prices:
         pass
-    
     print(f"Book {num} is {B} at the price of {P}")
     num += 1
     if num == len(Book_titles) and len(Book_prices):
         break
     
 
-
-    
-
-#print(f"Total Books with 2 star-rating {num}") # if you wanna see how books has 5 stars
+#print(f"Total Books with 5 star-rating {num}") # if you wanna see how books has 5 stars
 
